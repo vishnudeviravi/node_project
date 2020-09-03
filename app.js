26057969d7fbb2099b06248938617249d0acc86f
vishnudeviravi <ivishnudr@gmail.com>
2020-09-03 23:22:08 +0530

    Added simple styling to the page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const express = require('express');
+const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')))
 app.use('/admin',adminRoutes);
 app.use('/',shopRoutes);
 app.use((req,res,next)=>{
